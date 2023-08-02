4cc7c45870588b7fa142478c7ee275966dfbfca0
gogum <gogum@DESKTOP-KMH35MK>
2023-08-02 21:46:34 +0900

    첫날 git 1

---
+++ testPro/src/testPro/MainShot.java
@@ -0,0 +1,10 @@
+package testPro;
+
+public class MainShot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ testPro/src/testPro/MainStart.java
@@ -0,0 +1,12 @@
+package testPro;
+
+public class MainStart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(12233);
+		System.out.println(12233);
+	}
+
+}
